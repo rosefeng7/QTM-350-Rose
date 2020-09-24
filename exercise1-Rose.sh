7d5b69308d4ea4098b878b13e1d5976661237a9c
rosefeng7 <65143929+rosefeng7@users.noreply.github.com>
2020-09-24 15:31:39 -0400

    Add files via upload

---
+++ exercise1-Rose.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Author: Rose Feng
+# takes as input one argument, a directory, and outputs the number of different permission types in this directory.
+
+ls -l $1 | cut -d ' ' -f 1 | tail - -lines=+2 | uniq | wc -l
